aca95f3134373a6343720c4546e033dd1e6c3fa3
Kamen Yanakiev <kamen_yanakiev@epam.com>
2020-06-02 16:34:56 +0300

    task 2 done

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './file.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-first-webpack.bundle.js'
+    },
+    rules: [{
+        loader: "babel-loader",
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader"
+          },
+        options: {
+            rootMode: "upward",
+        }
+    }]
+};
