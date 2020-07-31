23cbd00584e19e6e138cebacccc5131fd047c6be
kamen-yanakiev <kamen_yanakiev@epam.com>
2020-07-31 14:52:36 +0300

    changes on AddMovieModal.test.js

--- src/components/header/AddMovieModal.test.js
+++ src/components/header/AddMovieModal.test.js
@@ -1,57 +1,53 @@
 import React from 'react';
-import { Provider, useDispatch } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from '../../context/movies/reducers';
 import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { mount, shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import AddMovieModal from './AddMovieModal';
+import { startAddMovie } from '../../context/movies/actions';
 //---------WIP---------//
-
-jest.mock('react-redux', () => ({
-    useDispatch: jest.fn(),
-}))
-
-// const dispatch = useDispatch();
-
-const store = createStore(reducer);
 
 Enzyme.configure({ adapter: new Adapter() });
 
+jest.mock('react-redux', () => ({
+    useDispatch: () => jest.fn(),
+}));
+
+jest.mock('../../context/movies/actions', () => ({
+    startAddMovie: (newTitle) => jest.fn(() => Promise.resolve({ Title: newTitle })),
+}));
+
+const setState = jest.fn();
+const useStateSpy = jest.spyOn(React, "useState");
+useStateSpy.mockImplementation((init) => [init, setState]);
+
 describe('AddMovieModal', () => {
-    // let wrapper;
-    const setState = jest.fn();
-    const useStateSpy = jest.spyOn(React, "useState")
-    useStateSpy.mockImplementation((init) => [init, setState]);
+    let wrapper;
 
     beforeEach(() => {
-        // wrapper = Enzyme.mount(Enzyme.shallow(<Provider store={store}><AddMovieModal /></Provider>).get(0))
-        // wrapper = shallow(<Provider store={store}><AddMovieModal /></Provider>)
-    });
-
-    describe("Title input", () => {
-        it("Should capture title correctly onChange", () => {
-
-            // const wrapper = mount(<Provider store={store}><AddMovieModal /></Provider>).get(0);
-            const wrapper = shallow(<AddMovieModal />).get(0);
-            // const wrapper = shallow(<AddMovieModal />);
-            // let input = shallow(wrapper.find('movie-title'));
-            // let input = wrapper.dive().find('#movie-title');
-            // const title = wrapper.find(AddMovieModal);
-            expect(wrapper).toMatchSnapshot();
-
-            console.log(wrapper);
-            // expect(wrapper.find('movie-title').value).toBe('');
-            // wrapper.find('movie-title').simulate('change', { target: { name: 'movie-title', value: '02' } });
-            // expect(wrapper.find('movie-title').value).toBe('02');
-
-            // input.instance().value = "Test";
-            // input.simulate("change");
-            // expect(setState).toHaveBeenCalledWith("Test");
-        });
+        wrapper = shallow(<AddMovieModal startAddMovie={ startAddMovie } />);
+        // wrapper = shallow(<AddMovieModal />);
     });
 
     afterEach(() => {
         jest.clearAllMocks();
-        // wrapper = null;
     });
+
+    describe("Title input", () => {
+        it("Should match the snapshot", () => {
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it("Should change values in inputs properly", () => {
+            wrapper.find('input#movie-title').simulate('change', {target: {value: 'New Title'}});
+            const submitBtn = wrapper.find('Button').at(2);
+            // console.log(wrapper.find('input#movie-title').html());
+            // console.log(submitBtn.html());
+            // let btnSpy = jest.spyOn(submitBtn, 'startAddMovie');
+            submitBtn.simulate('click');
+            expect(startAddMovie).toHaveBeenCalledTimes(1);
+            // expect(submitBtn).toHaveBeenCalledWith(expect.any(Function), {
+            //     Title: 'New Title',
+            // });
+        });
+    });
+
 });
