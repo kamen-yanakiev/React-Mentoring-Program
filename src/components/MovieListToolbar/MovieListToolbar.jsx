57e74658d9bacf6c76c294f02d21848dafd9838e
kamen-yanakiev <kamen_yanakiev@epam.com>
2020-07-31 09:32:20 +0300

    added query params for sorting, filtering is in progress

--- src/components/MovieListToolbar/MovieListToolbar.jsx
+++ src/components/MovieListToolbar/MovieListToolbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
+import { createBrowserHistory } from 'history';
 import { sortMoviesBy, filterMoviesBy } from '../../context/movies/actions';
 import sortTypes from '../../constants/sorting';
 import filterTypes from '../../constants/filtering';
@@ -8,23 +9,58 @@ import { sortBySelector, filterBySelector } from '../../context/movies/selectors
 import { useEffect } from 'react';
 
 const MainContentTop = () => {
+  const history = createBrowserHistory();
   const dispatch = useDispatch();
   const sortBy = useSelector(sortBySelector);
   const filterBy = useSelector(filterBySelector);
 
   const changeSortType = (event) => {
-
+    history.push(`/movies?${event.target.value.toLowerCase()}`)
     dispatch(sortMoviesBy(event.target.value));
   };
 
   const changeFilterType = (filterType) => {
-    dispatch(filterMoviesBy(filterType))
+    // history.push( {
+    //   search: `${history.location.search}&${filterType.toLowerCase()}`
+    // });
+    dispatch(filterMoviesBy(filterType));
   };
 
   useEffect(() => {
-    console.log('in use effect');
+    if (!history.location.search.includes(sortTypes.RELEASE_DATE.toLowerCase()) && !history.location.search.includes(sortTypes.RATING.toLowerCase()) && 
+    !history.location.search.includes(sortTypes.ALPHABETICALLY.toLowerCase())) {
+      history.push(`/movies?${sortTypes.RELEASE_DATE.toLowerCase()}`);
+    }
+
+    if (history.location.search.includes(sortTypes.RELEASE_DATE.toLowerCase())) {
+      dispatch(sortMoviesBy(sortTypes.RELEASE_DATE));
+    } else if (history.location.search.includes(sortTypes.RATING.toLowerCase())) {
+      dispatch(sortMoviesBy(sortTypes.RATING));
+    } else if (history.location.search.includes(sortTypes.ALPHABETICALLY.toLowerCase())) {
+      dispatch(sortMoviesBy(sortTypes.ALPHABETICALLY));
+    }
+
+    // if (!history.location.search.includes(filterTypes.ALL.toLowerCase()) && !history.location.search.includes(filterTypes.DOCUMENTARY.toLowerCase()) && 
+    // !history.location.search.includes(filterTypes.COMEDY.toLowerCase())  && !history.location.search.includes(filterTypes.HORROR.toLowerCase())  && 
+    // !history.location.search.includes(filterTypes.CRIME.toLowerCase())) {
+    //   history.push( {
+    //     search: `${history.location.search}&${filterTypes.ALL.toLowerCase()}`
+    //   });
+    // }
+    // debugger
+    // if (history.location.search.includes(filterTypes.ALL.toLowerCase())) {
+    //   dispatch(filterMoviesBy(filterTypes.ALL));
+    // } else if (history.location.search.includes(filterTypes.DOCUMENTARY.toLowerCase())) {
+    //   dispatch(filterMoviesBy(filterTypes.DOCUMENTARY));
+    // } else if (history.location.search.includes(filterTypes.COMEDY.toLowerCase())) {
+    //   dispatch(filterMoviesBy(filterTypes.COMEDY));
+    // } else if (history.location.search.includes(filterTypes.HORROR.toLowerCase())) {
+    //   dispatch(filterMoviesBy(filterTypes.HORROR));
+    // } else if (history.location.search.includes(filterTypes.CRIME.toLowerCase())) {
+    //   dispatch(filterMoviesBy(filterTypes.CRIME));
+    // }
  
-  }, [filterBy])
+  }, [filterBy, dispatch, history])
 
   return (
     <div className='main-content-top'>
